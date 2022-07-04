1b67bfd3dac83603ab53487aa23d3ecc1f851c96
utkarshaajtak <104076186+utkarshaajtak@users.noreply.github.com>
2022-07-04 22:53:31 +0530

    Initial

--- app/src/main/java/com/example/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/example/androidtutorial/MainActivity.kt
@@ -1,40 +1,37 @@
 package com.example.androidtutorial
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-  lateinit  var findBeer:Button
-   lateinit var brands:TextView
-   lateinit var beerColor :Spinner
+    private lateinit  var findBeer:Button
+    private lateinit var brands:TextView
+    private lateinit var beerColor :Spinner
 
-    private fun initVariables(): Unit {
-        findBeer = findViewById(R.id.find_beer)
-        brands = findViewById(R.id.brands)
-        beerColor=  findViewById(R.id.beer_color)
+    private fun initVariables() {
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initVariables()
-        findBeer.setOnClickListener {
-            val color =  beerColor.selectedItem
-            brands.text = getBeer(color.toString())
-        }
+//        initVariables()
+//        findBeer.setOnClickListener {
+//            val color =  beerColor.selectedItem
+//            brands.text = getBeer(color.toString())
+//        }
     }
 
-    private fun getBeer(beerColor: String): String {
-        return  when (beerColor){
-            "Light" -> listOf<String>("Jail pale ale","Lager Lite")
-            "Amber" -> listOf<String>("Jack Amber","Red moose")
-            "Brown" -> listOf<String>( "Brown Bear Beer","Bock Brownie")
-            else -> listOf<String>("Gout Stout","Dark Daniel")
-        }.reduce { acc, s -> acc+"\n" + s }
-    }
+//    private fun getBeer(beerColor: String): String {
+//        return  when (beerColor){
+//            "Light" -> listOf("Jail pale ale","Lager Lite")
+//            "Amber" -> listOf("Jack Amber","Red moose")
+//            "Brown" -> listOf( "Brown Bear Beer","Bock Brownie")
+//            else -> listOf("Gout Stout","Dark Daniel")
+//        }.reduce { acc, s -> acc+"\n" + s }
+//    }
 }
