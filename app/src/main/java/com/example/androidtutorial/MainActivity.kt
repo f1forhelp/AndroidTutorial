697770f040d528c700bd554a0cb736de3a718975
Utkarsh Sharma <rickysharma1111@gmail.com>
2023-01-26 11:56:21 +0530

    Some minor changes

--- app/src/main/java/com/example/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/example/androidtutorial/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.androidtutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
